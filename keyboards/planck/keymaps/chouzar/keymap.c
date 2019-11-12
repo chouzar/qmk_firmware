f64c40b004aaad13725ae6390ff472599c2fbeff
chouzar <wychouza@gmail.com>
2019-11-11 23:44:22 -0800

    Modified some command keys

--- keyboards/planck/keymaps/chouzar/keymap.c
+++ keyboards/planck/keymaps/chouzar/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2015-2017 Jack Humbert
+/* Copyright 2015sdjfdlsa;fjal-2017 Jack Humbert
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -58,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * |Tab   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  []  |
+ * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Back |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |Esc   |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |   '  |
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |   '  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -68,15 +68,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,     KC_Q,    KC_W,    KC_E,     KC_R,  KC_T,       KC_Y,      KC_U,   KC_I,       KC_O,    KC_P,    TD(BRACKETS),
-    KC_ESC,     KC_A,    KC_S,    KC_D,     KC_F,  KC_G,       KC_H,      KC_J,   KC_K,       KC_L,    KC_SCLN, KC_QUOTE,
+    KC_ESC,     KC_Q,    KC_W,    KC_E,     KC_R,  KC_T,       KC_Y,      KC_U,   KC_I,       KC_O,    KC_P,    KC_BSPACE,
+    KC_TAB,     KC_A,    KC_S,    KC_D,     KC_F,  KC_G,       KC_H,      KC_J,   KC_K,       KC_L,    KC_SCLN, KC_QUOTE,
     TD(LSHIFT), KC_Z,    KC_X,    KC_C,     KC_V,  KC_B,       KC_N,      KC_M,   KC_COMM,    KC_DOT,  KC_UP,   TD(RSHIFT),
-    INTERACT,   KC_LCTL, KC_LGUI, KC_LALT,  LOWER, KC_SPACE,   KC_SPACE,  RAISE,  TD(SLASH),  KC_LEFT, KC_DOWN, KC_RIGHT
+    INTERACT,   KC_LCTL, KC_LALT, KC_LGUI,  LOWER, KC_SPACE,   KC_SPACE,  RAISE,  TD(SLASH),  KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 
 /* INTERACT
  * ,-----------------------------------------------------------------------------------.
- * |   `  |      |      |      |      |      |      |      |   =  |  +-  | * /  | Bcks |
+ * |   `  |      |      |      |      |      |      |      |   =  |  +-  | * /  |   ´  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |      |  ()  |  []  |  {}  |  |>  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_INTERACT] = LAYOUT_planck_grid(
-  KC_GRV,  _______, _______, _______, _______,  _______,  _______, _______,          KC_EQL,       TD(SUM),      TD(MULT),   KC_BSPACE,
+  KC_GRV,  _______, _______, _______, _______,  _______,  _______, _______,          KC_EQL,       TD(SUM),      TD(MULT),   KC_LBRACKET,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    _______, TD(PARENTHESIS),  TD(BRACKETS), TD(CURLY),    TD(PIPE),   KC_ENTER,
   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   _______, KC_QUESTION,      KC_EXCLAIM,   KC_MS_BTN1,   KC_MS_UP,   KC_MS_BTN2,
   _______, _______, KC_F11,  KC_F12,  _______,  _______,  _______, _______,          XXXXXXX,      KC_MS_LEFT,   KC_MS_DOWN, KC_MS_RIGHT
